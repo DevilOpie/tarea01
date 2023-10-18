435983c4c197bbabd1b462c759974cdcce4a938c
Javier <javielnumero2@hotmail.com>
2023-10-18 09:22:16 +0200

    Initial Commit

---
+++ juegoIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  juegoIOS
+//
+//  Created by Javier González on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
